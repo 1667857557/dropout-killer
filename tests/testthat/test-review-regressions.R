6dd16b1bf7df58b109d68e28aa7e8380c4705af2
Yu-Feng Huang <54109903+1667857557@users.noreply.github.com>
2026-08-24 21:08:45 +0800

    Test sparse Matrix dispatch without attachment

--- tests/testthat/test-review-regressions.R
+++ tests/testthat/test-review-regressions.R
@@ -9,3 +9,23 @@ test_that("Codex review regressions remain fixed", {
                                               "cap_quantile", "seed", "return_score"))
   expect_gt(match("recovery_method", workflow), match("return_score", workflow))
 })
+
+test_that("sparse Matrix summary dispatch does not require Matrix attachment", {
+  was_attached <- "package:Matrix" %in% search()
+  if (was_attached) detach("package:Matrix", character.only = TRUE)
+  on.exit(if (was_attached) suppressPackageStartupMessages(library(Matrix)), add = TRUE)
+
+  x <- Matrix::sparseMatrix(i = c(1L, 2L), j = c(2L, 1L), dims = c(2L, 2L))
+  dimnames(x) <- list(c("g1", "g2"), c("c1", "c2"))
+  empty_mask <- Matrix::sparseMatrix(i = integer(), j = integer(), dims = dim(x)) > 0
+  expect_silent(recover_dropout_expression(x, empty_mask, c(1, 1)))
+
+  xn <- Matrix::sparseMatrix(i = c(1L, 2L), j = c(2L, 1L), x = c(2, 3), dims = c(2L, 2L),
+                             dimnames = dimnames(x))
+  mask <- Matrix::sparseMatrix(i = 1L, j = 1L, x = TRUE, dims = dim(xn))
+  d <- recover_dropout_expression(xn, mask, c(1, 1), min_target_observed = 10,
+                                  return_details = TRUE)
+  expect_equal(nrow(d$events), 1L)
+  expect_equal(d$events$i, 1L)
+  expect_equal(d$events$j, 1L)
+})
